36e437e1b35216b6c37d2e5a28f0f6f6ef5b76b1
MariaC30 <103943442+MariaC30@users.noreply.github.com>
2023-02-12 23:24:01 -0500

    Adicionando forms de clientes y tables

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,20 +1,26 @@
+import { CrearClientePageModule } from './home/clientes/crear-cliente/crear-cliente.module';
+import { ClientesPageModule } from './home/clientes/clientes.module';
 import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
+  {path: '', redirectTo: 'login', pathMatch: 'full'},
+  {path: 'home',  loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)},
+
+  {path: 'login', loadChildren: () => import('./login/login.module').then( m => m.LoginPageModule)},
   {
-    path: 'home',
-    loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)
-  },
-  {
-    path: '',
-    redirectTo: 'login',
-    pathMatch: 'full'
-  },
-  {
-    path: 'login',
-    loadChildren: () => import('./login/login.module').then( m => m.LoginPageModule)
-  },
+    path: 'clientes',
+    children: [
+      {
+        path: "",
+        loadChildren: () => import('./home/clientes/clientes.module').then(m => m.ClientesPageModule)
+      },
+      {
+        path: "crear-cliente",
+        loadChildren: () => import('./home/clientes/crear-cliente/crear-cliente.module').then(m=> m.CrearClientePageModule)
+      }
+    ]
+  }
 
 ];
 
